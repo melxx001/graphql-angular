36ac3d45d6e9055957ed64d781651ee686d5e1b4
Hicham El Hammouchi <hicham.elhammouchi@capitalone.com>
2017-12-29 04:25:40 -0500

    chore: Mutations - Creating Links

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { GraphQLModule } from './apollo.config';
 import { AppComponent } from './app.component';
+import { CreateLinkComponent } from './create-link/create-link.component';
 import { LinkItemComponent } from './link-item/link-item.component';
 import { LinkListComponent } from './link-list/link-list.component';
 
 @NgModule({
-  declarations: [AppComponent, LinkItemComponent, LinkListComponent],
-  imports: [BrowserModule, GraphQLModule],
+  declarations: [AppComponent, LinkItemComponent, LinkListComponent, CreateLinkComponent],
+  imports: [BrowserModule, FormsModule, GraphQLModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ angular-app/src/app/create-link/create-link.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+
+import { CREATE_LINK_MUTATION, CreateLinkMutationResponse } from '../graphql';
+
+@Component({
+  selector: 'app-create-link',
+  templateUrl: './create-link.component.html',
+  styleUrls: ['./create-link.component.css']
+})
+export class CreateLinkComponent implements OnInit {
+  description = '';
+  url = '';
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {}
+
+  createLink() {
+    this.apollo
+      .mutate({
+        mutation: CREATE_LINK_MUTATION,
+        variables: {
+          description: this.description,
+          url: this.url
+        }
+      })
+      .subscribe(response => {});
+  }
+}
