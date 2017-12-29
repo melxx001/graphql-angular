e1428e397ce006434460ff82f04fcad94a94ae7a
Hicham El Hammouchi <hicham.elhammouchi@capitalone.com>
2017-12-29 03:28:34 -0500

    chore: Queries - Loading link-items - part 1

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { GraphQLModule } from './apollo.config';
+import { AppComponent } from './app.component';
+import { LinkItemComponent } from './link-item/link-item.component';
+import { LinkListComponent } from './link-list/link-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LinkItemComponent, LinkListComponent],
   imports: [BrowserModule, GraphQLModule],
   providers: [],
   bootstrap: [AppComponent]
